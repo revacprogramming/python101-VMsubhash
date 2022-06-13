5a3d02d977f243e565f64d370485113f83d94eea
justitroyal <95460069+VMsubhash@users.noreply.github.com>
2022-06-13 01:48:09 +0000

    ...

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,3 +1,20 @@
+class Menu:
+    def __init__(self):
+      return None
+    def add(self,id,value):
+      if id in self.foods:
+        self.foods[id]+=value
+      else:
+        self.foods[id]=value
+      return None
+    def show(self):
+      c=list(self.foods().copy())
+      for i in c:
+        print(i[0],i[1])
+m = Menu()
+m.add("idly", 10)
+m.add("vada". 20)
+m.show()
 
 
 class Menu:
