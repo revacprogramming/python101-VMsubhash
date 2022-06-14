fb046f168b19af24e57a84b0b63f4eaff97bcad3
justitroyal <95460069+VMsubhash@users.noreply.github.com>
2022-06-14 17:22:42 +0000

    ,

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,9 +1,19 @@
-
-
 class Menu:
-  
+  def __init__(self):
+    self.foods={}
+  def __getitem__(self, key):
+      return self.foods[key]
+  def __setitem__(self, key, value):
+      if(key in self.foods):
+        self.foods[key]+=value
+      else:
+        self.foods[key]=value
+  def __str__(self):
+    k=""
+    for a in self.foods:
+      k+=(f"{a} => {self.foods[a]}\n")
+    return(k[:-1])
 m = Menu()
 m["idly"] = 10
 m["vada"] = 20
-
 print(m)
--- ActivitySet#2/problem#9.py
+++ ActivitySet#2/problem#9.py
@@ -1,13 +1,33 @@
-
-
 class Menu(dict):
-    """fill in class definition here"""
-
-
+  def __init__(self):
+    self.foods={}
+  def __getitem__(self, key):
+      return self.foods[key]
+  def __setitem__(self, key, value):
+      if(key in self.foods):
+        self.foods[key]+=value
+      else:
+        self.foods[key]=value
+  def __str__(self):
+    k=""
+    for a in self.foods:
+      k+=(f"{a} => {self.foods[a]}\n")
+    return(k[:-1])
 class Order:
-    """fill in class definition here"""
-
-
+    def __init__(self,Menu):
+      self.ord={}
+      self.foods=Menu.foods
+      return(None)
+    def __setitem__(self, key,quant):
+      if (self.foods[key]-quant)>=0:
+        self.ord[key]=quant
+      else:
+        print("the request is over the quantity")
+    def __str__(self):
+      k=""
+      for a in self.ord:
+        k+=(f"{a} => {self.ord[a]}\n")
+      return(k[:-1])
 class Bill:
     """fill in class definition here"""
 
@@ -22,7 +42,8 @@ try:
     o["pongal"] = 2
 
 except KeyError as e:
-    print(e)
-
+    print(str"e"+" is not available")
+print(o)
+'''
 b = Bill(m, o)
-print(b)
+print(b)'''
